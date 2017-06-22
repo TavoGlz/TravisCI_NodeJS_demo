d2b4119412fd2f9e673780bb4067e9df0bfb78d4
Tavo Glz <gusgonz@mx1.ibm.com>
2017-06-22 15:27:38 -0500

    removed semi-colon

--- hello.js
+++ hello.js
@@ -6,7 +6,8 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   //Presetting a string as a response.
   //Atention!! Missing semi-colon will fail the build (on purpose of course).
-  res.end('Hello World\n');
+  //Remove the semi-colon.
+  res.end('Hello World\n')
   //Setting the port on which our server will be listening through
 }).listen(1337, '127.0.0.1');
 //Printing a message in console log showing our server is already running
